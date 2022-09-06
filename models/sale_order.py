8f601c35d74d67c6ece7f4caab09d15dbddc3cc6
jeffrepo <jeffer.leo93@gmail.com>
2022-09-06 17:20:05 -0600

    update cost

--- models/sale_order.py
+++ models/sale_order.py
@@ -15,7 +15,7 @@ class SaleOrder(models.Model):
                 for line in sale.order_line:
                     logging.warning(line.product_id.create_analytic_sale)
                     if line.product_id.create_analytic_sale:
-                        analytic_move_dic = {'name': sale.name, 'account_id': sale.analytic_account_id.id, 'date': datetime.date.today(), 'amount': line.price_subtotal * -1, 'product_id': line.product_id.id }
+                        analytic_move_dic = {'name': sale.name, 'account_id': sale.analytic_account_id.id, 'date': datetime.date.today(), 'amount': line.product_id.standard_price * -1, 'product_id': line.product_id.id }
                         analytic_move_id = self.env['account.analytic.line'].create(analytic_move_dic)
                         logging.warning(analytic_move_id)
         return super(SaleOrder, self).action_confirm()
